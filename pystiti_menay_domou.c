b1c2ba18bfebeaa36875aa7b095366c277ad01ac
unknown <vanstepanov@yandex.ru>
2023-09-28 17:54:19 +0300

    New

---
+++ pystiti_menay_domou.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+    int a,b;
+    scanf("%d %d", &a, &b);
+    printf("%lf\n", (double)(a * a + b * b)/2);
+    printf("%lf\n", (double)(fabs(a) + fabs(b))/2);
+    return 0;
+}
